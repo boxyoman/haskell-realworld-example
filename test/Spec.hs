d1c9e464e3e486f2346cbcb133c43ace6a460fc7
jonny <jonny.covert@gmail.com>
2025-02-04 14:02:49 -0500

    Made a test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,19 @@
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE OverloadedStrings #-}
-module Main (main) where
+import System.FilePath.Glob (glob)
+import Test.DocTest (doctest)
+import Test.Hspec
+import Test.Hspec (hspec)
+import Test.Hspec.QuickCheck
+import Types qualified as T
+
+
 
 
 main :: IO ()
-main = print "No Tests right now"
-
+main = do
+  hSetBuffering stdout LineBuffering
+  hspec $ do
+    describe "slug" $ do
+      it "title to slug" $ do
+        let title = "Hello World"
+        let slug = "hello-world"
+        T.mkSlug (T.Title title) `shouldBe` T.Slug slug
